e494332c6fecf4324388245d69e405c54c6b3648
Norman Way-Long Chong <nwchong@ecelinux2.uwaterloo.ca>
2018-07-11 15:33:08 -0400

    added 3xsum_of_all_inputs entity for edge detector

--- kirsch.vhd
+++ kirsch.vhd
@@ -6,6 +6,44 @@ use ieee.numeric_std.all;
 use work.util.all;
 use work.kirsch_synth_pkg.all;
 
+entity three_x_sum is 
+  port(
+    i_val      : in unsigned ( 9 downto 0);
+    i_multiply : in std_logic;
+    i_reset    : in std_logic;
+    o_result   : out unsigned ( 13 downto 0);
+    o_overflow : out std_logic;
+    clk        : in std_logic
+  );
+end entity;
+architecture main of three_x_sum is 
+  signal sum	: unsigned (13 downto 0);
+begin
+  process is
+  begin 
+  wait until rising_edge(clk);
+  if (i_reset = '1') then
+      sum <= (others => '0');
+      o_overflow <= '0';
+  else
+      if (i_multiply <= '1') then
+          sum <= sum + shift_left (sum, 1);
+	  o_overflow <= '1' when sum (13 downto 12) > 0;
+      else
+      	  sum <= sum + i_val;
+      end if;
+  end if;
+  end process;
+  o_result <= sum;
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.util.all;
+use work.kirsch_synth_pkg.all;
+
 entity kirsch is
   port(
     clk        : in  std_logic;                      
@@ -17,7 +55,7 @@ entity kirsch is
     o_dir      : out direction_ty;
     o_mode     : out mode_ty;
     o_row      : out unsigned(7 downto 0);
-    o_col      : out unsigned(7 downto 0);
+    o_col      : out unsigned(7 downto 0)
   );  
 end entity;
 
@@ -26,3 +64,4 @@ architecture main of kirsch is
 begin  
   
 end architecture;
+
